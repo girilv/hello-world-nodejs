7327d31a5365106c568bdbb227324ab0dc1206c2
girilv <girilv@gmail.com>
2021-09-05 19:19:18 +0530

    for jenkins

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const process = require('process');
 
 var expressapp = express()
 expressapp.get('/', function (req, res) {
-    res.send('{"message":"Hello World Javascript Nuvepro v1 from Jenkins"}')
+    res.send('{"message":"Hello World Javascript Nuvepro v1 from Azure pipeline now"}')
 })
 expressapp.listen(5000, function () {
     console.log('Ready on port 5000!')
